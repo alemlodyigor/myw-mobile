4357e6fe1d7d89b91a689df68c4445e1a202001a
alemlodyigor <szeregowygpim@gmail.com>
2025-03-04 14:40:18 +0100

    created Wind

---
+++ components/Wind.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Wind = () => {
+  return (
+    <View>
+      <Text>Wind</Text>
+    </View>
+  )
+}
+
+export default Wind
